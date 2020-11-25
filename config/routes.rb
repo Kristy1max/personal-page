b0eaefccafed789a2669cead5130c7aa3e0a700c
Kristina Zhizherina <diablito.yey@mail.ru>
2020-11-25 19:12:52 +0100

    new page + linked pages to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'home', to: 'pages#home'
+  get 'home', to: 'pages#home', as: :home
+  get 'projects', to: 'pages#projects'
 end
